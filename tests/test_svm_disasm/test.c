0fa9d10c6d76e26b294aca26e54ba7f005407c34
Federico Cristina <federico.cristina@outlook.it>
2026-01-02 13:08:35 +0100

    Add SVM disassembler unit test

---
+++ tests/test_svm_disasm/test.c
@@ -0,0 +1,31 @@
+﻿#include "pi/svm/disasm.h"
+
+int main(void)
+{
+    pi_InitSystem();
+
+    PiSvmChunk chunk;
+
+    piInitSvmChunk(&chunk);
+
+    piSvmChunkWriteShortOp(&chunk, PI_SVM_OP_LDC, 0x00);
+    piSvmChunkWriteShortOp(&chunk, PI_SVM_OP_LDC, 0x01);
+    piSvmChunkWriteOp(&chunk, PI_SVM_OP_ADD);
+
+    piSvmChunkWriteShortOp(&chunk, PI_SVM_OP_LDC, 0x00);
+    piSvmChunkWriteShortOp(&chunk, PI_SVM_OP_LDI, 0x08);
+    piSvmChunkWriteOp(&chunk, PI_SVM_OP_NEG);
+    piSvmChunkWriteOp(&chunk, PI_SVM_OP_SUB);
+
+    piDisasmSvmChunk(stdout, &chunk, "TestChunk");
+    
+    piSvmChunkWriteOp(&chunk, PI_SVM_OP_INVALID);
+
+    piDisasmSvmChunk(stdout, &chunk, "TestChunk");
+
+    piFreeSvmChunk(&chunk);
+
+    pi_FreeSystem();
+
+    return EXIT_SUCCESS;
+}
